5b1c4830b4dbd0dc5e4a0f9c0d728569a5dd33c2
Anubhav Singh <asing110@stevens.edu>
2023-06-15 07:14:30 -0400

    added login and upload file endpoints

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, File, UploadFile
 from . import models
 from .routes import router
 from .config import engine
+from typing import Annotated
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(router, prefix="/passengers", tags=["passengers"])
+
+@app.post("/login")
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile | None = None):
+    if not file:
+        return{"message": "No file sent"}
+    else:
+        return{"filename": file.filename}

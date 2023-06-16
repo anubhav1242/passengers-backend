d81e456493770a1d3b6df3fe92a9088bf3431443
Anubhav Singh <asing110@stevens.edu>
2023-06-16 09:33:42 -0400

    Added upload file csv validation

--- app/main.py
+++ app/main.py
@@ -10,15 +10,3 @@ app = FastAPI()
 
 app.include_router(router, prefix="/passengers", tags=["passengers"])
 
-@app.post("/login")
-async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-    return {"username": username}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile | None = None):
-    if not file:
-        return{"message": "No file sent"}
-    else:
-        return{"filename": file.filename} 
-        
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, HTTPException, Path, Form, UploadFile
 from fastapi import Depends
 from .config import SessionLocal
 from sqlalchemy.orm import Session
 from .schemas import PassengerSchema, Request, Response, RequestPassenger
-
+from typing import Annotated
 from . import crud
 
 router = APIRouter()
@@ -17,7 +17,7 @@ def get_db():
         db.close()
 
 
-@router.post("/create")
+@router.post("/")
 async def create_passenger_service(request: RequestPassenger, db: Session = Depends(get_db)):
     crud.create_passenger(db, passengers=request.parameter)
     return Response(status="Ok",
@@ -37,7 +37,7 @@ async def get_passengers(skip: int = 0, limit: int = 500, db: Session = Depends(
     return Response(status="Ok", code="200", message="Success fetch all data", result=_passengers)
 
 
-@router.patch("/update")
+@router.put("/")
 async def update_passenger(request: RequestPassenger, db: Session = Depends(get_db)):
     _passengers = crud.update_passenger(db, passenger_id=request.parameter.id, Survived=request.parameter.Survived, Pclass=request.parameter.Pclass, Name=request.parameter.Name, Sex=request.parameter.Sex, Age=request.parameter.Age, SibSp=request.parameter.SibSp, Parch=request.parameter.Parch, Ticket=request.parameter.Ticket, Fare=request.parameter.Fare, Cabin=request.parameter.Cabin, Embarked=request.parameter.Embarked)
     return Response(status="Ok", code="200", message="Success update data", result=_passengers)
@@ -47,3 +47,17 @@ async def update_passenger(request: RequestPassenger, db: Session = Depends(get_
 async def delete_passenger(request: RequestPassenger,  db: Session = Depends(get_db)):
     crud.remove_passenger(db, passenger_id=request.parameter.id)
     return Response(status="Ok", code="200", message="Success delete data").dict(exclude_none=True)
+
+
+@router.post("/login")
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username}
+
+
+@router.post("/uploadfile/")
+async def create_upload_file(csvfile: UploadFile | None = None):
+    if csvfile.content_type == "text/csv":
+        return Response(status="ok", code="200", message="Success upload file")
+    else:
+        return Response(status="Not ok", code="415", message="Please upload a csv file")
+        
